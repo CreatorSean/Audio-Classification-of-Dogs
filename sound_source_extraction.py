09b58fd4d7224a97f3bce65285bda3a826753f0a
SiHeon No <108929080+CreatorSean@users.noreply.github.com>
2024-01-12 16:17:43 +0900

    update : convert mp4 to mp3

--- sound_source_extraction.py
+++ sound_source_extraction.py
@@ -1,15 +1,25 @@
 from moviepy.editor import VideoFileClip
+from glob import glob
 
-def extract_audio_from_video(video_path, output_audio_path):
-    video = VideoFileClip(video_path)
-    audio = video.audio
-    audio.write_audiofile(output_audio_path)
+video_path = 'assets/mp4/*'  # 저장된 비디오 파일 경로
 
-video_path = 'assets/mp4/*'  # 비디오 파일 경로
-output_audio_path = 'extracted_audio.mp3'  # 추출된 오디오 저장 경로
+def extract_audio_from_video(video_paths:list):
+    for path in video_paths:
+        video_name = path.split("\\")[1].split(".")[0]
+        output_audio_path = "assets/mp3/"+video_name+".mp3" # 추출된 오디오 저장 경로
+        print(video_name+" 파일 오디오로 변환중...")
+        video = VideoFileClip(path)
+        audio = video.audio
+        audio.write_audiofile(output_audio_path)
+        print(output_audio_path+" 변환 완료!")
+
+def get_video_path_list():
+    video_paths = glob(video_path)
+    return video_paths
 
 def main():
-    extract_audio_from_video(video_path, output_audio_path)
+    video_paths = get_video_path_list()
+    extract_audio_from_video(video_paths)
 
 main()
 
