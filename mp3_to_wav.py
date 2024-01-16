a5bbc26bcaaa35c8c3cf3dfd755b966b3ddd7d1b
SiHeon No <108929080+CreatorSean@users.noreply.github.com>
2024-01-16 16:10:05 +0900

    fix :gitignore and wav

--- mp3_to_wav.py
+++ mp3_to_wav.py
@@ -4,15 +4,24 @@ from glob import glob
 # MP3 파일 경로
 mp3_file = 'assets/mp3/*'
 
-# WAV 파일 경로
-wav_file = 'assets/wav'
-
-# MP3 파일 로드
-audio = AudioSegment.from_mp3(mp3_file)
-
 # WAV로 변환하고 저장
-audio.export(wav_file, format='wav')
+def mp3_to_wav(mp3_paths:list):
+    for path in mp3_paths:
+        mp3_name = path.split("\\")[1].split(".")[0]
+        print(mp3_name+" 파일 wav로 변환중...")
+        audio = AudioSegment.from_mp3(path)
+        output_audio_path = "assets/wav/"+mp3_name+".wav" # 추출된 오디오 저장 경로
+        audio.export(output_audio_path, format='wav')
+        print(output_audio_path+" 변환 완료!")
 
-def get_video_path_list():
-    video_paths = glob(mp3_file)
-    return video_paths
+
+def get_mp3_path_list():
+    mp3_paths = glob(mp3_file)
+    return mp3_paths
+
+
+def main():
+    mp3_paths = get_mp3_path_list()
+    mp3_to_wav(mp3_paths)
+
+main()
