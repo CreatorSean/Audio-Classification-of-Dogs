ceecf293b9950a5458300929d542a2612753604c
SiHeon No <108929080+CreatorSean@users.noreply.github.com>
2024-01-29 16:49:40 +0900

    Update check_waveform.py

--- utils/check_waveform.py
+++ utils/check_waveform.py
@@ -3,7 +3,7 @@ import librosa.display
 import librosa
 
 # 오디오 파일 로드
-path = 'assets/wav/1.wav'  # WAV 파일의 경로를 지정하세요
+path = 'assets/wav/4.wav'  # WAV 파일의 경로를 지정하세요
 audio, sample_rate = librosa.load(path, sr=None)  # sr=None으로 설정하면 파일의 원래 샘플링 레이트를 사용합니다
 
 # waveform 시각화
