437289e88434221a85771d244eae52e0fa90e1ec
SiHeon No <108929080+CreatorSean@users.noreply.github.com>
2024-01-30 15:54:39 +0900

    Create sound_soucre_extraction_2.py

---
+++ utils/sound_soucre_extraction_2.py
@@ -0,0 +1,18 @@
+from moviepy.editor import VideoFileClip
+
+path = 'assets/mp4/159.mp4'
+
+def extract_audio_from_video(video_path):
+    video_name = video_path.split("/")[2].split(".")[0]
+    print(video_name)
+    output_audio_path = "assets/mp3/"+video_name+".mp3"
+    print(video_name+" 파일 오디오로 변환중...")
+    video = VideoFileClip(video_path)
+    audio = video.audio
+    audio.write_audiofile(output_audio_path)
+    print(output_audio_path+" 변환 완료!")
+
+def main():
+    extract_audio_from_video(path)
+
+main()
