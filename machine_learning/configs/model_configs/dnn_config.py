6afc1e6b4936691e464dfdfa2f374abb41673e80
SiHeon No <108929080+CreatorSean@users.noreply.github.com>
2024-02-13 13:47:25 +0900

    add : dnn_config

---
+++ machine_learning/configs/model_configs/dnn_config.py
@@ -0,0 +1,8 @@
+dnn_config = {
+    "hidden_layer_sizes": (256, 512, 512, 256, 1),
+    "solver": "lbfgs",
+    "max_iter": 2000,
+    "learning_rate": "adaptive",
+    "alpha": 0.001,
+    "activation": "tanh",
+}
